697a0a1cbfe1093ee75adbaa07aef55a88cb9cd4
Paula Rehbein <prehbein0@gmail.com>
2024-07-12 15:40:25 -0300

    Conteudo sprint06

---
+++ Sprint06/3.Evidências/Desafio/parte1.py
@@ -0,0 +1,71 @@
+import boto3
+from botocore.exceptions import ClientError
+import logging
+
+def user(profilename):                 #função que pede a cahve de acesso do usuário e diz o serviço
+    session=boto3.Session(profile_name=profilename)
+    s3=session.client('s3')
+    return s3
+
+
+def criar_bucket(nome, s3):    #função criar bucket, parâmetro nome do bucket
+    try:
+        s3.create_bucket(Bucket=nome)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+def list_buckets(s3):             #função lisatr buckets
+    try:
+        lista_buckets=[]
+        s3_client = s3
+        response = s3_client.list_buckets()
+        for bucket in response['Buckets']:  #Para cada bucket, fazemos um looping para exibir os dados.
+            lista_buckets.append(bucket['Name'])    #adiciona os nomes dos buckets numa lista
+        return lista_buckets      
+    except ClientError as e:
+        print(e)
+    
+   
+def upload_object(file_name, bucket, s3, object_name):
+    if object_name is None:
+        object_name = file_name
+
+    try:
+        response = s3.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+      
+    return True
+
+
+#administradoracess 
+profilename=input("Profile_name: ")
+s3=user(profilename)                #variavel s3
+
+
+#variaveis para criar bucket
+nomebucket=input("Nome do bucket: ")
+
+
+buckets_existentes=list_buckets(s3)
+if nomebucket in buckets_existentes:
+    print("Bucket já existe.")
+else:
+    criar_bucket(nomebucket, s3)
+    print("Bucket criado. ")
+
+
+#quantidade de objetos que serão adicionados no bucket
+quant=int(input(f"Quantidade de objetos que quer adicionar no Bucket {nomebucket}: "))
+
+
+for n in range (quant):
+    #variaveis para adicionar objeto no bucket
+    nome_chave=input(f"Informe o caminho do objeto: ")
+    name=input("Nome do objeto: ")
+    upload_object(nome_chave, nomebucket, s3, name)
+    print(f"Objeto {name} adicionado ao bucket {nomebucket}. ")
---
+++ Sprint06/README.md
@@ -0,0 +1,14 @@
+# Sprint 06
+## Conteúdo da Sprint:
+&nbsp;&nbsp;&nbsp; A Sprint 06 foi composta por diversos cursos de preparação da AWS, por atividades no console da AWS, pela definição do assunto do desafio final e pelo desafio da Sprint que compõem a primeira parte do desafio final. <p>
+
+## Atividades: 
+&nbsp;&nbsp;&nbsp; Haviam três atividades para serem realizadas no console da AWS. Uma sobre Laboratório S3, outra sobre Laboratório Athena, e a última sobre Laboratório Lambda.
+
+## Desafio final - Assunto de análise:
+&nbsp;&nbsp;&nbsp;Antes do início da primeira parte do desafio é necessário definir sobre o que vai ser a análise final e o assunto de foco do desafio. Os assuntos que vou desenvolver serão:
+* Que idade os atores e atrizes tem quando fazem filmes/séries bem avaliados? Com quais gêneros que eles se destacaram com essa idade?
+* Quais os filmes de Sci-FI mais bem avaliados nos anos 2000 (2000-2009)? Qual a porcentagem de mulheres que trabalharam nesses filmes?
+
+## Desafio:
+&nbsp;&nbsp;&nbsp; O desafio então foi formado pela ingestão batch dos dados. Onde era necessário criar um bucket e fazer upload de objetos a esse bucket, isso por meio de um script Python, que por fim seria executado em um container no docker.
